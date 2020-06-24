58914efd37df7d20d15fa1c3b51da8b8cd5eb5ce
sincaster <sindavmi@gmail.com>
2020-06-24 21:13:44 +0300

    added cool stuff

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = addListeners;
+
+function addListeners(){
+    document.getElementById('welcome').addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+}
+
+function mouseUp()
+{
+    window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e){
+  window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e){
+    var div = document.getElementById('welcome');
+  div.style.position = 'absolute';
+  div.style.top = e.clientY + 'px';
+  div.style.left = e.clientX + 'px';
+} 
